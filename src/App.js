1492f8e9d0abdec1877b2f6ad190356b3685c481
Sean <sean@seanmizen.com>
2021-08-17 19:39:35 +0100

    Added spacer as an entire feature lol

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ExampleFeature from "./features/ExampleFeature";
 import ItemList from "./features/ItemList";
+import Spacer from "./features/Spacer";
 import styles from "./App.module.css";
 
 //todo https://adrianroselli.com/2018/02/github-contributions-chart.html
@@ -9,7 +10,9 @@ function App() {
   return (
     <div>
       <h1>seanmizen.com</h1>
+      <Spacer />
       <p>developer | automator | person | he/him</p>
+      <Spacer />
       <ItemList />
     </div>
   );
