94d6e98968220ad3587501526c84edcafed65663
seanmizen <sean@seanmizen.com>
2021-08-18 20:02:16 +0100

    fixed capitalisation

--- src/features/Projects/Projects.js
+++ src/features/Projects/Projects.js
@@ -3,8 +3,8 @@ import "./Projects.module.css";
 function Projects() {
   return (
     <div>
-      <div>No featured projects yet - stay tuned for more!</div>
-      <div>Last updated 2021-08-18</div>
+      <div>no featured projects yet - stay tuned for more.</div>
+      <div>last updated 2021-08-18</div>
     </div>
   );
 }
