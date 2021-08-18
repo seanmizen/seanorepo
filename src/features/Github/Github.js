c0e3bc2e2df63cae102cdb7cb0e59b237d63ecc0
seanmizen <sean@seanmizen.com>
2021-08-18 19:06:29 +0100

    Cleansing and removal of ExampleFeature

---
+++ src/features/Github/Github.js
@@ -0,0 +1,14 @@
+//import { useState } from "react";
+import styles from "./Github.module.css";
+
+function Github() {
+  return (
+    <div>
+      <a href="https://github.com/smizzle-probably">
+        github.com/smizzle-probably
+      </a>
+    </div>
+  );
+}
+
+export default Github;
