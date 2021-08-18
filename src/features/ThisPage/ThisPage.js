379b0c029aad5bc39eeeb49d5c482cac62ba28b1
Sean <sean@seanmizen.com>
2021-08-18 23:17:40 +0100

    fixed capitalisation

--- src/features/ThisPage/ThisPage.js
+++ src/features/ThisPage/ThisPage.js
@@ -2,6 +2,7 @@ import styles from "./ThisPage.module.css";
 import Collapsible from "react-collapsible";
 import ColorPicker from "./components/ColorPicker.js";
 import Glow from "./components/Glow";
+import Todo from "./components/Todo";
 //import { ChromePicker } from "react-color"; -- redacted: this. Semi-useful library but let's stick with the default <input> instead.
 
 function ThisPage() {
@@ -14,7 +15,12 @@ function ThisPage() {
       </li>
       <li className="unselected" selected={false}>
         <Collapsible trigger="glow">
-          <Glow text={"Oh look, I'm glowing"} />
+          <Glow text={"oh look, i'm glowing"} />
+        </Collapsible>
+      </li>
+      <li className="unselected" selected={false}>
+        <Collapsible trigger="todo">
+          <Todo />
         </Collapsible>
       </li>
     </ul>
