da451329d32f34e0eba702412514df03a05cb101
seanmizen <sean@seanmizen.com>
2021-08-18 23:17:45 +0100

    Added Todo

---
+++ src/features/ThisPage/components/Todo.js
@@ -0,0 +1,14 @@
+function Todo() {
+  return (
+    <div>
+      <ul>
+        <li>implement some server-side code</li>
+        <li>add a database</li>
+        <li>make some web requests</li>
+        <li>make the ui better (?)</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
