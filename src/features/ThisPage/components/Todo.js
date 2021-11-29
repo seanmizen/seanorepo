3a7e714003eca2dcbdd0b6b05836ac04b7424fcd
seanmizen <sean@seanmizen.com>
2021-11-29 09:38:28 +0000

    ..

--- src/features/ThisPage/components/Todo.js
+++ src/features/ThisPage/components/Todo.js
@@ -5,6 +5,8 @@ function Todo() {
         <li>implement some server-side code</li>
         <li>add a database</li>
         <li>make some web requests</li>
+        <li>github contribution chart</li>
+        <li>accessibility: fix tabbing</li>
         <li>make the ui better (?)</li>
       </ul>
     </div>
