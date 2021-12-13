fc5f9bdb325a70832297397211fb3417ee311068
seanmizen <sean@seanmizen.com>
2021-12-13 23:37:38 +0000

    removed glow

--- src/features/ThisPage/ThisPage.js
+++ src/features/ThisPage/ThisPage.js
@@ -1,7 +1,6 @@
 import "./ThisPage.module.css";
 import Collapsible from "react-collapsible";
 import ColorPicker from "./components/ColorPicker.js";
-import Glow from "./components/Glow";
 import Todo from "./components/Todo";
 //import { ChromePicker } from "react-color"; -- redacted: this. Semi-useful library but let's stick with the default <input> instead.
 
@@ -11,11 +10,6 @@ function ThisPage() {
       <li className="unselected" selected={false}>
         <Collapsible trigger="colors">
           <ColorPicker />
-        </Collapsible>
-      </li>
-      <li className="unselected" selected={false}>
-        <Collapsible trigger="glow">
-          <Glow text={"oh look, i'm glowing"} />
         </Collapsible>
       </li>
       <li className="unselected" selected={false}>
