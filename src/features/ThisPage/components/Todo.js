e2c329f0b48e538d3e5835aa66fc6fe4d4d2675d
Sean Mizen <sean@seanmizen.com>
2022-02-01 14:42:18 +0000

    marked tabbing as complete

--- src/features/ThisPage/components/Todo.js
+++ src/features/ThisPage/components/Todo.js
@@ -6,7 +6,6 @@ function Todo() {
         <li tabIndex={0}>add a database</li>
         <li tabIndex={0}>make some web requests</li>
         <li tabIndex={0}>github contribution chart</li>
-        <li tabIndex={0}>accessibility: fix tabbing</li>
         <li tabIndex={0}>make the ui better (?)</li>
       </ul>
     </div>
