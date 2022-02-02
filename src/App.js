f2cd57368d19d29f167c758ab75ae07a014e206d
seanmizen <sean@seanmizen.com>
2022-02-02 13:09:28 +0000

    fixed CI problem omg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import Apps from "./pages/Apps";
 import React from "react";
 import "./App.module.css"; //default css
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // todo https://adrianroselli.com/2018/02/github-contributions-chart.html
 // Comment for the sake of github workflows.
 
