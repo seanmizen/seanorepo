72632c0ed2e2eb6cd57327fcca7a5b8fbe88c9b4
Sean Mizen <sean@seanmizen.com>
2022-02-02 13:28:51 +0000

    added extra projects

--- src/features/Projects/Projects.js
+++ src/features/Projects/Projects.js
@@ -14,6 +14,29 @@ function Projects() {
         </a>{" "}
         - a flashcarding app built in React
       </div>
+      <div>
+        <a
+          tabIndex={0}
+          role="listitem"
+          aria-label="URL for this website"
+          href={process.env.PUBLIC_URL}
+        >
+          seanmizen.com
+        </a>{" "}
+        - this site!
+      </div>
+      <div>
+        <a
+          tabIndex={0}
+          role="listitem"
+          aria-label="URL for Tourguide-ar, a tourguide web app for universities. (a group work project)"
+          href="https://tourguide-ar.github.io/Tourguide-ar/"
+        >
+          tourguide-ar
+        </a>{" "}
+        - a tourguide web app for universities (a WIP group project)
+      </div>
+
       <div>last updated 2022-01-27</div>
     </div>
   );
