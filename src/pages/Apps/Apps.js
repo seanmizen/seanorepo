7ab57fce4abbba113db63ed0da3a74bb680cacbd
seanmizen <sean@seanmizen.com>
2022-02-02 18:11:42 +0000

    moved some padding - fixed, basically

--- src/pages/Apps/Apps.js
+++ src/pages/Apps/Apps.js
@@ -8,7 +8,9 @@ function Apps() {
     <div>
       <h1 className={styles["no-animation"]}>seanmizen.com</h1>
       <h2 className={styles[""]}>current projects:</h2>
-      <Projects />
+      <div className={styles["padded-left"]}>
+        <Projects />
+      </div>
       {/* TODO move Spacer / HomeLink into a nav-style component which is on all pages but Home */}
       <Spacer />
       <HomeLink />
