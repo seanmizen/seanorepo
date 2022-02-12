cbf1125125fe74eb506ba3ef01f6ddc0dc60cda2
Sean Mizen <sean@seanmizen.com>
2022-02-12 15:46:44 +0000

    messing around with code snippets and dark theme

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // todo https://adrianroselli.com/2018/02/github-contributions-chart.html
 // Comment for the sake of github workflows.
 
+// Types for colour scheme:
+// Saved mode
+// type Mode = "light" | "dark" | "system";
+// // Visual themes
+// type Theme = "light" | "dark";
+
+// const ThemeContext = React.createContext<{
+//   mode: Mode;
+//   theme: Theme;
+//   setMode: (mode);
+// }>({
+//   mode: "system",
+//   theme: "light",
+//   setMode: () => {}
+// });
+
 function App() {
   return (
     <Router basename={process.env.REACT_APP_BASENAME}>
