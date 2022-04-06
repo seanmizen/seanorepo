85855eb333656a0d83432c05cf752c7187006909
seanmizen <sean@seanmizen.com>
2022-04-06 10:34:31 +0100

    Added clickable marker to li

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,19 @@
 import styles from "./Home.module.css";
-import Collapsible from "react-collapsible";
 import { Projects, ThisPage, Donate, Github } from "../../features";
 import { Spacer, LastUpdated, ThemeToggle } from "../../components";
 import { Link } from "react-router-dom";
 import { ThemeContext } from "../../Theme";
+import Collapsible from "react-collapsible";
 import React from "react";
 
 function Home() {
   const { mode, toggleMode } = React.useContext(ThemeContext);
+
+  const clickableMarker = (e) => {
+    // Allows clicking the <li> marker to activate the collapsible
+    e.target.nextSibling.children[0].click();
+  };
+
   return (
     <div className={"container"}>
       <h1 className="test" alt="shaunmizen.com">
@@ -17,6 +23,9 @@ function Home() {
       <Spacer />
       <ul className={styles["home-list"]}>
         <li>
+          <div onClick={clickableMarker} className={styles["clickable-marker"]}>
+            {"\xa0"}
+          </div>
           <Collapsible transitionTime="100" trigger="projects" tabIndex={0}>
             <div className={styles["sublink"]}>
               <Link to="/apps">visit</Link>
@@ -25,16 +34,25 @@ function Home() {
           </Collapsible>
         </li>
         <li>
+          <div onClick={clickableMarker} className={styles["clickable-marker"]}>
+            {"\xa0"}
+          </div>
           <Collapsible transitionTime="100" trigger="github" tabIndex={0}>
             <Github />
           </Collapsible>
         </li>
         <li>
+          <div onClick={clickableMarker} className={styles["clickable-marker"]}>
+            {"\xa0"}
+          </div>
           <Collapsible transitionTime="100" trigger="donate" tabIndex={0}>
             <Donate />
           </Collapsible>
         </li>
         <li>
+          <div onClick={clickableMarker} className={styles["clickable-marker"]}>
+            {"\xa0"}
+          </div>
           <Collapsible transitionTime="100" trigger="this page" tabIndex={0}>
             <ThisPage />
           </Collapsible>
