adfee838c0ba6a638a6cbbed902ade0f697e1211
seanmizen <sean@seanmizen.com>
2024-12-06 10:37:20 +0000

    workspace upate, yarn install, move seed data

--- apps/carolinemizen.art/src/server/src/middleware/errors.ts
+++ apps/carolinemizen.art/src/server/src/middleware/errors.ts
@@ -1,21 +1,26 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
 
 const router = express.Router();
 
 const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
   res.status(404);
-  if (req.accepts('json')) {
-    res.json({ error: 'Not found' });
+  if (req.accepts("json")) {
+    res.json({ error: "Not found" });
     return;
   }
-  res.type('txt').send('Not found');
+  res.type("txt").send("Not found");
 };
 
-const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
     message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
   });
 };
 
