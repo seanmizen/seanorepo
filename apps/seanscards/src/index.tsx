392ee0dcf6dd446d365e61872b3e7b24773669b2
seanmizen <sean@seanmizen.com>
2024-12-13 20:49:16 +0000

    seanscards - mega loads of everything

--- apps/seanscards/src/index.tsx
+++ apps/seanscards/src/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import { darkTheme } from "./theme";
+// import { ThemeProvider } from "@mui/material";
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
+      {/* <ThemeProvider theme={darkTheme}>
+        <CssBaseline /> */}
       <App />
-    </React.StrictMode>,
+      {/* </ThemeProvider> */}
+    </React.StrictMode>
   );
 }
