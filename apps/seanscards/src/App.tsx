ed925f559384d6d76b0f19ec7149078a2c1dc5f7
seanmizen <sean.mizen@globacap.com>
2024-12-13 17:29:54 +0000

    seanscards initial commit

---
+++ apps/seanscards/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+/**
+ * haha. they said we couldn't do it. they said it was impossible.
+ * but we did it. a single-page application. with a single page.
+ */
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Sean's Cards</h1>
+      <p>Send your friends a card!</p>
+      <p>Send your mother a card!</p>
+      <p>Send your enemies a card!</p>
+      <p>Send your dog a card!</p>
+    </div>
+  );
+};
+
+export default App;
+
+// videos:
+// "enterprising young men - star trek" and just writing the stuff
