fb610c9efd0276c097cf70d569ec183df7ac9d23
seanmizen <sean.mizen@globacap.com>
2024-12-15 13:35:57 +0000

    somesuch things

--- apps/seanscards/backend/index.ts
+++ apps/seanscards/backend/index.ts
@@ -5,6 +5,12 @@ import cookie from "@fastify/cookie";
 import jwt from "@fastify/jwt";
 import { Server, IncomingMessage, ServerResponse } from "http";
 import Stripe from "stripe";
+import { configs } from "../configs";
+import { randomUUID } from "crypto";
+
+const config = configs[process.env.NODE_ENV || "development"];
+const appDomain = config.appBasename; // "http://localhost:4000" or "https://seanscards.com"
+
 const stripe = new Stripe(
   "sk_test_51QVX2JBsGhYF8YEWi3iM9PCLwFMG2AMbKx1eq6L4mPMp6TB62S9tve5NypbQmeiTTJ9epEAJhaO01lTLOZI4Huxy0009gNLP2Z"
 );
@@ -23,13 +29,12 @@ fastify.register(cookie, {
 fastify.register(jwt, { secret: "super" });
 
 // fastify.register(routes);
-fastify.get("/", async (request, reply) => {
+fastify.get("/api", async (request, reply) => {
   console.debug("we hello-worlding", request.headers);
   return { hello: "world" };
 });
 
-const YOUR_DOMAIN = "http://localhost:3000"; // uhh
-fastify.post("/create-checkout-session", async (request, reply) => {
+fastify.post("/api/create-checkout-session", async (request, reply) => {
   console.debug("create-checkout-session", request.headers);
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
@@ -41,28 +46,28 @@ fastify.post("/create-checkout-session", async (request, reply) => {
       },
     ],
     mode: "payment",
-    return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+    return_url: `${appDomain}/return?session_id={CHECKOUT_SESSION_ID}`,
     automatic_tax: { enabled: true },
   });
 
   reply.send({ clientSecret: session.client_secret });
 });
 
-const calculateOrderAmount = (items) => {
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  let total = 0;
-  items.forEach((item) => {
-    total += item.amount;
-  });
-  return total;
-};
+// const calculateOrderAmount = (items) => {
+//   // Calculate the order total on the server to prevent
+//   // people from directly manipulating the amount on the client
+//   let total = 0;
+//   items.forEach((item) => {
+//     total += item.amount;
+//   });
+//   return total;
+// };
 
-interface PaymentIntentRequestBody {
-  items: { amount: number }[];
-}
+// interface PaymentIntentRequestBody {
+//   items: { amount: number }[];
+// }
 
-// fastify.post("/create-payment-intent", async (request, reply) => {
+// fastify.post("/api/create-payment-intent", async (request, reply) => {
 //   const { items } = request.body as PaymentIntentRequestBody;
 
 //   // Create a PaymentIntent with the order amount and currency
@@ -82,7 +87,7 @@ interface PaymentIntentRequestBody {
 //   });
 // });
 
-fastify.get("/session-status", async (request, reply) => {
+fastify.get("/api/session-status", async (request, reply) => {
   const session = await stripe.checkout.sessions.retrieve(
     (request.query as unknown as any).session_id
   );
@@ -91,6 +96,48 @@ fastify.get("/session-status", async (request, reply) => {
     status: session.status,
     customer_email: session.customer_details?.email,
   });
+});
+
+fastify.get("/api/session-token", async (request, reply) => {
+  // BE does not care nor store sessions. this is an FE responsibility
+  reply.send(randomUUID());
+});
+
+// method: "POST",
+// headers: {
+//   "Content-Type": "application/json",
+// },
+// body: JSON.stringify({
+//   sessionToken,
+//   ...formik.values,
+// }),
+// })
+// .then((res) => res.json())
+// .then((data) => {
+//   console.log("form response:", data);
+// })
+// .catch((error) => {
+//   console.error("Error submitting form:", error);
+// });
+
+// update-session-fields
+type FormShape = {
+  message: string;
+  address: string;
+  email: string;
+  // plus sessionToken! (string)
+};
+
+fastify.post("/api/update-session-fields", async (request, reply) => {
+  const { sessionToken, ...fields } = request.body as FormShape & {
+    sessionToken: string;
+  };
+
+  // BE does not care nor store sessions. this is an FE responsibility
+  console.log("sessionToken", sessionToken);
+  console.log("fields", fields);
+
+  reply.send({ status: "ok" });
 });
 
 const reset = "\x1b[0m";
