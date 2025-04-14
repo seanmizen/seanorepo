a229861d75548eda8f1a9d4acbc034a94ec60b8b
Sean Mizen <sean@seanmizen.com>
2025-04-14 17:05:29 +0100

    dockerise seanmizen.com

--- apps/seanmizen.com/src/components/LastUpdated/LastUpdated.jsx
+++ apps/seanmizen.com/src/components/LastUpdated/LastUpdated.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import styles from './LastUpdated.module.css';
+import styles from "./LastUpdated.module.css";
 
-import toWords from 'number-to-words/src/toWords';
+import { toWords } from "number-to-words";
 
-const comfyTime = dateTime => {
+const comfyTime = (dateTime) => {
   const hour = dateTime.getUTCHours();
   const mins = dateTime.getUTCMinutes();
   const nextHour = hour + 1;
-  let descriptor = 'in the day';
+  let descriptor = "in the day";
   switch (hour) {
     case 1:
     case 2:
@@ -16,18 +16,18 @@ const comfyTime = dateTime => {
     case 4:
     case 5:
     case 6:
-      descriptor = 'in the early morning';
+      descriptor = "in the early morning";
       break;
     case 7:
     case 8:
     case 9:
     case 10:
     case 11:
-      descriptor = 'in the morning';
+      descriptor = "in the morning";
       break;
     case 12:
       // special case: midday
-      return 'at midday';
+      return "at midday";
     case 13:
     case 14:
     case 15:
@@ -35,31 +35,31 @@ const comfyTime = dateTime => {
     case 17:
     case 18:
     case 19:
-      descriptor = 'in the afternoon';
+      descriptor = "in the afternoon";
       break;
     case 20:
     case 21:
     case 22:
-      descriptor = 'at night';
+      descriptor = "at night";
       break;
     default:
       // special case: midnight
-      return 'around midnight';
+      return "around midnight";
   }
 
-  let hourStr = '';
-  let nextHourStr = '';
+  let hourStr = "";
+  let nextHourStr = "";
   if (hour === 12) {
-    hourStr = 'midday';
+    hourStr = "midday";
   } else if (hour === 24) {
-    hourStr = 'midnight';
+    hourStr = "midnight";
   } else {
     hourStr = toWords(((hour - 1) % 12) + 1);
   }
   if (nextHour === 12) {
-    nextHourStr = 'midday';
+    nextHourStr = "midday";
   } else if (nextHour === 24) {
-    nextHourStr = 'midnight';
+    nextHourStr = "midnight";
   } else {
     nextHourStr = toWords(((nextHour - 1) % 12) + 1);
   }
@@ -84,7 +84,9 @@ const comfyTime = dateTime => {
 };
 
 function LastUpdated({ apiRepoUrl }) {
-  const [lastUpdated, setLastUpdated] = useState("at um, well, i'm not sure yet");
+  const [lastUpdated, setLastUpdated] = useState(
+    "at um, well, i'm not sure yet"
+  );
 
   const sendRequestForDate = async () => {
     try {
@@ -96,9 +98,11 @@ function LastUpdated({ apiRepoUrl }) {
       const updatedDate = new Date(repo.pushed_at);
       const comfortableTime = comfyTime(updatedDate);
 
-      setLastUpdated(`${updatedDate.toISOString().slice(0, 10)} ${comfortableTime}`);
+      setLastUpdated(
+        `${updatedDate.toISOString().slice(0, 10)} ${comfortableTime}`
+      );
     } catch (error) {
-      console.error('Failed to fetch repository data:', error);
+      console.error("Failed to fetch repository data:", error);
     }
   };
 
@@ -106,7 +110,9 @@ function LastUpdated({ apiRepoUrl }) {
     sendRequestForDate();
   }, []);
 
-  return <div className={styles['last-updated']}>last updated {lastUpdated}</div>;
+  return (
+    <div className={styles["last-updated"]}>last updated {lastUpdated}</div>
+  );
 }
 
 export default LastUpdated;
