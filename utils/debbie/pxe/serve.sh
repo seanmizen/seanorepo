00fabafc3f3b8d9fcdd68e65a40f0bc67bdabd42
Sean Mizen <sean@seanmizen.com>
2025-04-20 19:17:08 +0100

    misc

--- utils/debbie/pxe/serve.sh
+++ utils/debbie/pxe/serve.sh
@@ -12,3 +12,5 @@ sudo dnsmasq --conf-file="$CONFIG" --no-daemon &
 
 echo "Starting HTTP server on port 8000..."
 python3 -m http.server 8000 --directory .
+# to listen for responses:
+# sudo tcpdump -i en8 port 67 or port 69 or port 4011 or port 8000
