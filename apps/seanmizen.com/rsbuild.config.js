5d4f9e26ee14a66129ef12628ac7851184ed0c53
Sean Mizen <sean@seanmizen.com>
2025-04-25 14:59:36 +0100

    various fixes and glsl stuff

--- apps/seanmizen.com/rsbuild.config.js
+++ apps/seanmizen.com/rsbuild.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
     template: "./public/index.html",
   },
   output: {
-    externals: {
-      // 'mapbox-gl': 'mapboxgl',
-    },
-    assetPrefix: "/rsbuild-plugin-glsl/",
+    assetPrefix: "/",
   },
 });
