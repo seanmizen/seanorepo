e0eb0bec8f5cf2b5da03a4c02de8e605a21a483d
seanmizen <sean@seanmizen.com>
2025-10-04 10:42:54 +0100

    Ip getter (#1)
    * ip-getter initial commit
    
    * make TCP getter
    
    * docker start
    
    * docker: dev works
    
    * docker: prod works
    
    * add gateway path
    
    * good! we working here
    
    * sense, add to docker registry

--- apps/seanmizen.com/src/pages/Home/Home.jsx
+++ apps/seanmizen.com/src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React, { useEffect } from "react";
+import { useState, useContext } from "react";
 
-import { HomeLi, LastUpdated, Spacer, ThemeToggle } from "../../components";
+import {
+  HomeLi,
+  LastUpdated,
+  Spacer,
+  ThemeToggle,
+  SSHModal,
+} from "../../components";
 import { Donate, Github, Projects, ThisPage, Xmas } from "../../features";
 import { ThemeContext } from "../../providers/Theme";
 import { ShaderSean } from "../../components/ShaderSean";
+import { useKeySequence } from "../../hooks";
 
 const Home = ({ setIsSnowing }) => {
-  const { mode, toggleMode } = React.useContext(ThemeContext);
+  const { mode, toggleMode } = useContext(ThemeContext);
+  const [isSSHModalOpen, setIsSSHModalOpen] = useState(false);
+
+  useKeySequence({
+    ssh: () => setIsSSHModalOpen(true),
+    poop: () => alert("oops, poop!"),
+  });
 
   const subsections = [
     { component: <Projects />, trigger: "projects", subLink: "/apps" },
@@ -38,6 +51,10 @@ const Home = ({ setIsSnowing }) => {
         <ShaderSean />
       </div>
       <ThemeToggle mode={mode} toggleMode={toggleMode} />
+      <SSHModal
+        isOpen={isSSHModalOpen}
+        onClose={() => setIsSSHModalOpen(false)}
+      />
     </div>
   );
 };
