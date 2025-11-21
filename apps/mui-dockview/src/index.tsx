f78b5da4e58461c5ba9f83a5b4f5ba64d8210918
Sean Mizen <sean@seanmizen.com>
2025-11-21 21:55:31 +0000

    mui dockview

---
+++ apps/mui-dockview/src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "dockview/dist/styles/dockview.css";
+import "./App.css";
+import { StrictMode } from "react";
+
+const rootEl = document.getElementById("root");
+if (rootEl) {
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
