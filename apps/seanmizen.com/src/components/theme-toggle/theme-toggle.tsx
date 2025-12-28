7a99db084ddf432c99b97b43c06362123cd36919
Sean Mizen <sean@seanmizen.com>
2025-12-28 22:44:11 +0000

    icon, not button

--- apps/seanmizen.com/src/components/theme-toggle/theme-toggle.tsx
+++ apps/seanmizen.com/src/components/theme-toggle/theme-toggle.tsx
@@ -1,5 +1,6 @@
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
 import type { ThemeMode } from '@/types';
+import { Auto, Dark, Light } from './icons';
 import styles from './theme-toggle.module.css';
 
 interface ThemeToggleProps {
@@ -7,18 +8,23 @@ interface ThemeToggleProps {
   toggleMode: () => void;
 }
 
+const iconMap: Record<ThemeMode, ReactNode> = {
+  system: <Auto />,
+  dark: <Dark />,
+  light: <Light />,
+};
+
 const ThemeToggle: FC<ThemeToggleProps> = ({ mode, toggleMode }) => {
   return (
     <div className={styles['theme-toggle-outer']}>
-      <div className={styles['theme-toggle-inner']}>
-        <button
-          type="button"
-          onClick={toggleMode}
-          aria-label={`Current theme: ${mode}. Click to change.`}
-        >
-          {`\xa0theme: ${mode}\xa0`.replace('system', 'auto')}
-        </button>
-      </div>
+      <button
+        type="button"
+        onClick={toggleMode}
+        aria-label={`Current theme: ${mode}. Click to change.`}
+        className={styles['theme-toggle-button']}
+      >
+        {iconMap[mode]}
+      </button>
     </div>
   );
 };
