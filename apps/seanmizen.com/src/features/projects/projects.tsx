e9cd8fa042802d511e061e8b684425e19093867c
Sean Mizen <sean@seanmizen.com>
2025-12-30 00:18:45 +0000

    hugh mungus changes

--- apps/seanmizen.com/src/features/projects/projects.tsx
+++ apps/seanmizen.com/src/features/projects/projects.tsx
@@ -21,6 +21,18 @@ const projectList: Project[] = [
     arialabel: 'URL for this website',
   },
   {
+    linklabel: 'Planning Poker',
+    description: 'Planning Poker - Agile estimation tool for teams',
+    href: 'https://pp.seanmizen.com',
+    arialabel: 'URL for Planning Poker',
+  },
+  {
+    linklabel: 'carolinemizen.art',
+    description: 'Art portfolio (under construction)',
+    href: 'https://carolinemizen.art',
+    arialabel: 'URL for carolinemizen.art',
+  },
+  {
     linklabel: 'shist',
     description: "Sean's History Tool",
     href: 'https://github.com/seanmizen/shist',
